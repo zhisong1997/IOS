d40d2c21a3d0b67daaaa83862123a90a45344f05
tanzhisong <tanzhisong6079@e.neivce.edu.my>
2024-11-27 11:06:15 +0800

    week 4 done

--- week4/exercise1.6.playground/Contents.swift
+++ week4/exercise1.6.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let num = [1,2,3,4]
+
+
+for i in num {
+    for j in 1...i{
+        print("*", terminator:"")
+    }
+    print("")
+}
+var n = 3
+for i in 1...n {
+    for j in 1...n{
+        print("*", terminator:"")
+    }
+    print("")
+     n -= 1
+}
+
+
