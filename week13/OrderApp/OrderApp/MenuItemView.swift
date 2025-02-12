8c8bacd16df7ab60b0b59249f3dc114fddb58dea
zhisong1997 <tanzhisong6079@e.neivce.edu.my>
2025-02-12 11:39:28 +0800

    done13

---
+++ week13/OrderApp/OrderApp/MenuItemView.swift
@@ -0,0 +1,43 @@
+//
+//  MenuItemView.swift
+//  OrderApp
+//
+//  Created by labuser on 05/02/2025.
+//
+import SwiftUI
+
+struct MenuItemView: View {
+    @Bindable var item: MenuItem
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(item.imageName)
+                .resizable()
+                .scaledToFit()
+                
+            
+            Text(item.name)
+                .font(.headline)
+
+            Text("$\(item.price, specifier: "%.2f")")
+                .font(.subheadline)
+
+            Button(action: {
+                item.isAddedToOrder.toggle()
+            }) {
+                Text(item.isAddedToOrder ? "Remove" : "Add to Order")
+                    .font(.caption)
+                    .padding(6)
+                    .background(item.isAddedToOrder ? Color.red : Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    let newItem = MenuItem(name: "Pizza", type: "Food", price: 12.50, imageName:"Chicken")
+    MenuItemView(item: newItem)
+}
